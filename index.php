2640e264aa7affd42438c676379abffb877cb4ec
cuongtm1 <53479326+cuongtm1@users.noreply.github.com>
2019-11-25 15:02:19 +0700

    test

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+require_once './commons/utils.php';
+require_once './commons/helpers.php';
+require_once 'vendor/autoload.php';
+$url = isset($_GET['url']) ? $_GET['url'] : '/';
+require_once './bootstrap.php';
+use Routes\CustomRoute;
+CustomRoute::init($url);
+
+ ?>
